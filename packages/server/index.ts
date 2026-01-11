bca128f647d1b817fb2c7c43860dd1fa2145f34c
ratnadeepnayak <ratnadeepnayak@gmail.com>
2026-01-11 09:48:22 -0500

    chore: initial commit

---
+++ packages/server/index.ts
@@ -0,0 +1,15 @@
+
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const app = express();
+app.use(express.json());
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+   console.log(`Server is running on http://localhost:${port}`);
+});
